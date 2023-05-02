6ac750976a2876db398d8d8636be7861e6c78c28
freddie <matthew@drinking.co.uk>
2023-05-02 13:33:34 +0100

    added in Key to request catcher post

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ def process_json():
     time_str = data.get('Time', '')
     table_number = data.get('Table Number', 'NA')
     path = data.get('path', 'v1/a/drinking/d/a0bc35c9/q')  # New line to extract the path from the JSON data
-    api_key = data.get('APIKEY')  # New line to extract the API key from the JSON data
+    api_key = data.get('api_key')  # New line to extract the API key from the JSON data
 
     # Format the time string
     time = datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%H:%M')
