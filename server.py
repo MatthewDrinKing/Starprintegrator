e42378f12a3798cc1089927272e2e4dc2216f7ce
freddie <matthew@drinking.co.uk>
2023-07-04 21:32:09 +0100

    maybe a fix

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ def process_json():
 
     # Extract the relevant information from the JSON data
     time_str = data.get('Time', '')
-    table_number = data.get('table_number', 'NA')
+    table_number = data.get('table_number', 'NA') if 'table_number' in data else 'NA'
     path = data.get('path', 'v1/a/drinking/d/a0bc35c9/q')  # New line to extract the path from the JSON data
     api_key = data.get('api_key')  # New line to extract the API key from the JSON data
 
